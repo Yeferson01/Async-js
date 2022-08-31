cd0513b80216b3db667b9702558326bffa13ea57
Yeferson Cronos <yefersonyscc2@gmail.com>
2022-08-31 11:41:08 -0500

    testing to post with async function

--- promesas/challengePost.js
+++ promesas/challengePost.js
@@ -2,6 +2,7 @@ import fetch from "node-fetch";
 
 const API_URL = 'https://api.escuelajs.co/api/v1';
 
+/*
 function postData(urlApi, data) {
     const response = fetch(urlApi, {
         method: 'POST',
@@ -12,7 +13,7 @@ function postData(urlApi, data) {
         },
         body: JSON.stringify(data)
     });
-    return response
+    return response;
 }
 
 const data = {
@@ -25,4 +26,38 @@ const data = {
 
 postData(`${API_URL}/products`, data)
 .then(response => response.json())
-.then(data => console.log(data));
+.then(data => console.log(data)); */
+
+//Probando asyncs para post
+async function postDataAsync(urlApi, data) {
+    const response = await fetch(urlApi, {
+        method: 'POST',
+        mode: 'cors',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+    return response;
+}
+
+const data = {
+    "title": "Magic Pencil",
+    "price": 10,
+    "description": "To write and have a magic learning",
+    "categoryId": 5,
+    "images": ["https://thumbs.dreamstime.com/b/magic-pencil-21524249.jpg"]
+}
+
+async function anoterFn(urlApi, data) {
+    try {
+        const result = await postDataAsync(urlApi, data);
+        const all = await result.json();
+        console.log(all);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+anoterFn(`${API_URL}/products`, data);
