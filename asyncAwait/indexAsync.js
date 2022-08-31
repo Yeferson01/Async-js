c21b6b089925ff94c374fcea9e0c5b76e6512f61
Yeferson Cronos <yefersonyscc2@gmail.com>
2022-08-31 10:50:14 -0500

    Add comments to explain how async/await work

--- asyncAwait/indexAsync.js
+++ asyncAwait/indexAsync.js
@@ -1,3 +1,5 @@
+//Creamos una funcion anonima con arrow y retornamos una promesa, las promesas retornan una opcion
+//en base a una condicion 
 const fnasyns = () => {
     return new Promise((resolve, reject) => {
         (true) 
@@ -6,12 +8,16 @@ const fnasyns = () => {
     });
 }
 
+//Creamos otra funcion pero esta es async para especificar que es asincrona y puede hacer llamados await que no 
+//interrumpen la ejecucion del codigo
 const anotherFunction = async  () => {
+    //Aqui esperamos la respuesta de la promesa y la almacenamos en una variable
     const something  = await fnasyns();
     console.log(something);
     console.log('Hello');
 }
 
+//Hacemos el lladado despues de un log para comprobar que no se interrumpe la ejecucion
 console.log('Before');
 anotherFunction();
 console.log('After');
