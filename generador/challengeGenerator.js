ff4427f88509967a4b47bc5daacac259556e1832
Yeferson Cronos <yefersonyscc2@gmail.com>
2022-09-01 11:16:30 -0500

    fixing bugs

--- generador/challengeGenerator.js
+++ generador/challengeGenerator.js
@@ -4,7 +4,7 @@ import fetch from "node-fetch";
 const API_URL = 'https://api.escuelajs.co/api/v1';
 
 //Creamos la funcion asincrona que hara la conexion a fetch
-async function* fetchData(urlApi) {
+async function fetchData(urlApi) {
     //Hacemos un llamado await a fetch con la url de la api y almacenamos la respuesta en una constante
     const response = await fetch(urlApi);
     //Transformamos la respuesta anterior a json
@@ -14,7 +14,7 @@ async function* fetchData(urlApi) {
 }
 
 //Creamos otra funcion asincrona para manejar los errores de la conexion a la api y obtener los datos necesarios
-async function* anotherFn(urlApi) {
+const anotherFn = async function* (urlApi) {
     try {
         //Hacemos el primer lladado a la api y para obtener los productos 
         const products = await fetchData(`${urlApi}/products`);
@@ -22,14 +22,18 @@ async function* anotherFn(urlApi) {
         const product = await fetchData(`${urlApi}/products/${products[0].id}`);
         //Obtenemos la categoria del producto anterior 
         const category = await fetchData(`${urlApi}/categories/${product.category.id}`);
-        
-         yield console.log(products);
-         yield console.log(product.title);
-         yield console.log(category.name);
+
         //Imprimimos los datos obtenidos anteriormente, los producteos, el nombre de un producto en especifico y la cateoria del mismo
+        yield console.log(products);
+        yield console.log(product.title);
+        yield console.log(category.name);
     } catch (error) {
         yield console.error(error);
     }
 }
 
-anotherFn(API_URL);
+const result = anotherFn(API_URL);
+result.next();
+result.next();
+result.next();
+result.next();
